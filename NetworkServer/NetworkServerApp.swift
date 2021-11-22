401c4b34f27cb2e9410ee9a1273db569de979f1f
Mitar <m.mitrovic@itsinbox.com>
2021-11-22 15:50:14 +0100

    Initial Commit

---
+++ NetworkServer/NetworkServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServerApp.swift
+//  NetworkServer
+//
+//  Created by vesko on 22.11.21..
+//
+
+import SwiftUI
+
+@main
+struct NetworkServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
