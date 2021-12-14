2b0b7f4aa932e27b171692a18563f843a2edab2a
Mitar <m.mitrovic@itsinbox.com>
2021-12-14 14:11:14 +0100

    Display large image

--- NetworkServer/ServerConnection.swift
+++ NetworkServer/ServerConnection.swift
@@ -15,31 +15,34 @@ protocol ReceiveDelegate{
 @available(macOS 10.14, *)
 class ServerConnection{
     var transferDataDelegate: TransferData?
-//The TCP maximum package size is 64K 65536
-    let MTU = 251993    
-//    let MTU = 100000000
-
+    //The TCP maximum package size is 64K 65536
+    let MTU = 251993
+    //    let MTU = 100000000
+    
     private static var nextID: Int = 0
     let  connection: NWConnection
     let id: Int
-
+    var receivedDataCount: UInt32? = nil
+    var displayData = Data()
+    //    var inputStream: InputStream
+    
     init(nwConnection: NWConnection, transferData: TransferData) {
         connection = nwConnection
         id = ServerConnection.nextID
         ServerConnection.nextID += 1
         self.transferDataDelegate = transferData
-  
+        
     }
-
+    
     var didStopCallback: ((Error?) -> Void)? = nil
-
+    
     func start() {
         print("connection \(id) will start")
         connection.stateUpdateHandler = self.stateDidChange(to:)
         setupReceive()
         connection.start(queue: .main)
     }
-
+    
     private func stateDidChange(to state: NWConnection.State) {
         switch state {
         case .waiting(let error):
@@ -52,16 +55,44 @@ class ServerConnection{
             break
         }
     }
-
+    
     private func setupReceive() {
         connection.receive(minimumIncompleteLength: 1, maximumLength: MTU) { (data, _, isComplete, error) in
+            //            var inputStream: InputStream
             if let data = data, !data.isEmpty {
-//                let message = String(data: data, encoding: .utf8)
-//                self.dataMessage.message = data
-                self.transferDataDelegate?.onMessageReceive(data: data)
-//                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "passData"),
-//                                                object: nil, userInfo: ["info": data])
-//                self.send(data: data)
+                if self.receivedDataCount == nil {
+                    
+                    let count = data.withUnsafeBytes{
+                        [UInt8](UnsafeBufferPointer(start: $0, count: 4))
+                    }
+                    let cutingData = data
+                    let rawData = cutingData.dropFirst(4)
+
+                    let countData = Data(bytes: count)
+                    self.receivedDataCount = UInt32(bigEndian: countData.withUnsafeBytes { $0.pointee })
+
+                    self.displayData.append(rawData)
+
+                    if(self.receivedDataCount! > UInt32(data.count)){
+                    self.receivedDataCount = self.receivedDataCount! - UInt32(cutingData.count)
+                    }else{
+                        self.transferDataDelegate?.onMessageReceive(data: self.displayData)
+                        self.displayData = Data()
+                        self.receivedDataCount = nil
+                    }
+                    print(rawData)
+
+                }else if(self.receivedDataCount! > 0 && self.receivedDataCount! > UInt32(data.count)){
+
+                    self.displayData.append(data)
+                    self.receivedDataCount = self.receivedDataCount! - UInt32(data.count)
+                }else{
+                    
+                    self.transferDataDelegate?.onMessageReceive(data: self.displayData)
+                    self.displayData = Data()
+                    self.receivedDataCount = nil
+                }
+                
             }
             if isComplete {
                 self.connectionDidEnd()
@@ -72,11 +103,31 @@ class ServerConnection{
             }
         }
     }
-
-//     @objc func passMessageNotification(_ notification: Notification){
-//         print(notification)
-//     }
-
+    //    func extract(from data: inout Data) -> Data? {
+    //        guard data.count > 0 else {
+    //            return nil
+    //        }
+    //
+    //        // Define the length of data to return
+    //        let length = Int.init(data[0])
+    //
+    //        // Create a range based on the length of data to return
+    //        let range = Range(0..<length)
+    //
+    //        // Get a new copy of data
+    //        let subData = data.subdata(in: range)
+    //
+    //        // Mutate data
+    //        data.removeSubrange(range)
+    //
+    //        // Return the new copy of data
+    //        return subData
+    //    }
+    
+    //     @objc func passMessageNotification(_ notification: Notification){
+    //         print(notification)
+    //     }
+    
     func send(data: Data) {
         self.connection.send(content: data, completion: .contentProcessed( { error in
             if let error = error {
@@ -86,23 +137,23 @@ class ServerConnection{
             print("connection \(self.id) did send, data: \(data as NSData)")
         }))
     }
-
+    
     func stop() {
         print("connection \(id) will stop")
     }
-
-
-
+    
+    
+    
     private func connectionDidFail(error: Error) {
         print("connection \(id) did fail, error: \(error)")
         stop(error: error)
     }
-
+    
     private func connectionDidEnd() {
         print("connection \(id) did end")
         stop(error: nil)
     }
-
+    
     private func stop(error: Error?) {
         connection.stateUpdateHandler = nil
         connection.cancel()
